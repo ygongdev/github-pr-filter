111262a752434481f63cb119b54806e5db8d1c55
Yicheng Gong <ygongdev@gmail.com>
2021-07-24 09:18:13 -0700

    fix manifest.json and added more robust checks as to when to generate the filter

--- content-script.js
+++ content-script.js
@@ -5,84 +5,125 @@
   // Adds filterGlob to the global namespace;
   await import(src);
 
+  /**
+   * Globals
+   */
   let globString = "";
+  // Prevents toolbar creation from keep triggering mutation observer.
+  const WIDGET_ID = 'glob-filter-widget';
 
-  const toolbar = getPRToolbar();
-  const globWidget = createGlobFilterWidget();
-  toolbar.appendChild(globWidget);
+  // Because sometimes Github is changing page content and url without triggering a reload, so we need to use a mutation observer.
+  const filterObserver = new MutationObserver(debounce(run, 1000));
+  filterObserver.observe(document.body, { childList: true, subtree: true });
 
-  function getPRToolbar() {
-    const toolbarContainer = document.querySelector('.pr-toolbar');
-    const actions = toolbarContainer.querySelector('details');
-    const toolbar = actions.parentNode;
-    return toolbar;
-  }
+  run();
 
-  function createGlobFilterWidget() {
-    const widget = document.createElement('div');
-    const title = document.createElement('strong');
-    title.textContent = "Glob filter";
-    title.style.marginLeft = "8px";
-
-    const globInput = document.createElement('input');
-    globInput.type = 'text';
-    globInput.style.marginLeft = "8px";
-
-    const submit = document.createElement('button');
-    // Copying Github's class names
-    submit.className = "btn btn-sm btn-primary";
-    submit.textContent = "Run filter";
-    submit.style.marginLeft = "8px";
-
-    globInput.addEventListener('change', function() {
-      globString = this.value;
-    });
-
-    submit.addEventListener('click', function() {
-      /**
-       * TODO:
-       * 1. I think we can optimize this time complexity by using a dictionary.
-       * 2. I can we think can support multiple glob pattern in one string, similar to vscode search by splitting the string by "," and run `filterGlob` on each splitted output.
-       * Then we can merge the output filenames back together and remove duplicates by leveraging dictionary from 1). (e.g, globString = "globPattern1, globPattern2")
-       */
-      const changedFiles = [...getAllChangedFiles()];
-      const changedFilenames = changedFiles.map(getFilename);
-      const filteredFilenames = filterGlob(changedFilenames, globString);
-
-      // Get files that do not match the glob, so we can collapse them.
-      const filteredFiles = changedFiles.filter(file => {
-        const name = getFilename(file);
-        return filteredFilenames.indexOf(name) < 0;
-      });
-
-      filteredFiles.forEach(file => {
-        const button = getCollapseButton(file);
-
-        // If file is currently open, click to collapse it.
-        if (button.getAttribute('aria-expanded') === 'true') {
-          button.click();
-        }
-      })
-    });
-
-    widget.appendChild(title);
-    widget.appendChild(globInput);
-    widget.appendChild(submit);
-    return widget;
-  }
+  function debounce(fn, rate) {
+    let timer;
   
-  function getFilename(fileDOM) {
-    return fileDOM.querySelector('a').getAttribute('title');
+    return function() {
+      if (timer) {
+        clearTimeout(timer);
+        timer = null;
+      }
+      timer = setTimeout(() => {
+        fn.apply(this, arguments)
+      }, rate);
+    }
   }
 
-  function getAllChangedFiles() {
-    const filesContainer = document.querySelector('#files');
-    const files = filesContainer.querySelectorAll('[data-details-container-group]');
-    return files;
+  function isPRPage() {
+    // location.href doesn't seem to capture 'www.', so this pattern here is a bit different from the manifest.json
+    return filterGlob([location.href], '*://*github.com/**/pull/**/files*').length > 0 || filterGlob([location.href], '*://*githubprivate.com/**/pull/**/files*').length > 0;
   }
 
-  function getCollapseButton(fileDOM) {
-    return fileDOM.querySelector('button');
+  function run() {
+    if (!isPRPage()) {
+      return;
+    }
+
+    if (document?.querySelector(`#${WIDGET_ID}`)) {
+      return;
+    }
+
+    const toolbar = getPRToolbar();
+    const globWidget = createGlobFilterWidget();
+    toolbar.appendChild(globWidget);
+
+    function getPRToolbar() {
+      const toolbarContainer = document.querySelector('.pr-toolbar');
+      const actions = toolbarContainer.querySelector('details');
+      const toolbar = actions.parentNode;
+      return toolbar;
+    }
+  
+    function createGlobFilterWidget() {
+      const widget = document.createElement('div');
+      widget.id = WIDGET_ID;
+      const title = document.createElement('strong');
+      title.textContent = "Glob filter";
+      title.style.marginLeft = "8px";
+  
+      const globInput = document.createElement('input');
+      globInput.type = 'text';
+      globInput.style.marginLeft = "8px";
+  
+      const submit = document.createElement('button');
+      // Copying Github's class names
+      submit.className = "btn btn-sm btn-primary";
+      submit.textContent = "Run filter";
+      submit.style.marginLeft = "8px";
+  
+      globInput.addEventListener('change', function() {
+        globString = this.value;
+      });
+  
+      submit.addEventListener('click', function() {
+        /**
+         * TODO:
+         * 1. I think we can optimize this time complexity by using a dictionary.
+         * 2. I can we think can support multiple glob pattern in one string, similar to vscode search by splitting the string by "," and run `filterGlob` on each splitted output.
+         * Then we can merge the output filenames back together and remove duplicates by leveraging dictionary from 1). (e.g, globString = "globPattern1, globPattern2")
+         */
+        const changedFiles = [...getAllChangedFiles()];
+        const changedFilenames = changedFiles.map(getFilename);
+        const filteredFilenames = filterGlob(changedFilenames, globString);
+  
+        // Get files that do not match the glob, so we can collapse them.
+        const filteredFiles = changedFiles.filter(file => {
+          const name = getFilename(file);
+          return filteredFilenames.indexOf(name) < 0;
+        });
+  
+        filteredFiles.forEach(file => {
+          const button = getCollapseButton(file);
+  
+          // If file is currently open, click to collapse it.
+          if (button.getAttribute('aria-expanded') === 'true') {
+            button.click();
+          }
+        })
+      });
+  
+      widget.appendChild(title);
+      widget.appendChild(globInput);
+      widget.appendChild(submit);
+      return widget;
+    }
+    
+    function getFilename(fileDOM) {
+      return fileDOM.querySelector('a').getAttribute('title');
+    }
+  
+    function getAllChangedFiles() {
+      const filesContainer = document.querySelector('#files');
+      const files = filesContainer.querySelectorAll('[data-details-container-group]');
+      return files;
+    }
+  
+    function getCollapseButton(fileDOM) {
+      return fileDOM.querySelector('button');
+    }
   }
 })();
 
